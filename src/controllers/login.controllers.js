a9ffa63ccfe2a1a27e2e09e141d16d4de246cede
JAVIER ALEMAN <javier.aleman@gracialab.com.co>
2023-09-05 21:33:41 -0500

    login

--- src/controllers/login.controllers.js
+++ src/controllers/login.controllers.js
@@ -2,10 +2,14 @@ import { pool } from "../db/db.js";
 
 export const login = async (req, res) => {
   try {
+    let status = true;
     const { username, password } = req.body;
     const [rows] = await pool.query("SELECT `document`,`fullname`,`userprofile`,`phone`,`email` FROM `userapp` WHERE `username` = ? AND `password` = ?", 
     [ username, password])
-    res.json(rows[0]);
+    if (rows.length === 0) {
+      status = false;
+    }
+    res.json({status, data:rows});
   } catch (error) {
     console.log(error);
     return res.status(500).json({ message: "error realizar el login" });
