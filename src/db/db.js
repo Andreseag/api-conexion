df5bd6d23576b457c1807ffb2c1323d8b1d9c7d2
gitjaleman <javiersettex@gmail.com>
2023-09-13 08:17:13 -0500

    conexion

--- src/db/db.js
+++ src/db/db.js
@@ -11,9 +11,18 @@ export const pool = createPool({
 import pg from 'pg';
 const { Pool } = pg;
 export const pool = new Pool({
+  user: 'root',
+  host: 'dpg-ck09bdg21fec73e3db90-a',
+  database: 'conexiondb',
+  password: 'KRGrgmazEU5fs3gbFwKIpARvfdoRRh8s',
+  port: 5432, 
+});
+/*
+export const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
   database: 'conexiondb',
   password: '0',
   port: 5432, 
 });
+*/
